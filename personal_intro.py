c6f3d8a5781b31dfb9ccf9338480e00f7373d215
Pulivarthi Chandra Sekhar <chanduchandrasekhar27@gmail.com>
2025-12-14 18:36:42 +0530

    Update personal_intro.py

--- personal_intro.py
+++ personal_intro.py
@@ -1,1 +1,10 @@
+name = input("What is your name? ")
+age = input("How old are you? ")
+hobby = input("What is your favorite hobby? ")
+city = input("Which city are you from? ")
+
+print("Welcome", name, )
+print(f"You are {age} years old.")
+print(f"You love {hobby}.")
+print(f"You are from {city}.")
 
